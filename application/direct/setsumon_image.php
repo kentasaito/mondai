b4441391f625da86a29541453939aa7a6d1a73f7
Kenta Saito <ken77@gatedruid.com>
2016-05-04 18:40:39 +0900

    setsumon_image.phpのディレクトリトラバーサル対策

--- application/direct/setsumon_image.php
+++ application/direct/setsumon_image.php
@@ -1,5 +1,9 @@
 <?php
 
 $filename = htmlspecialchars($_GET['filename'], ENT_QUOTES);
+if (strpos($filename, '..') !== FALSE)
+{
+	exit();
+}
 header('Content-type: image');
 readfile("../../mondai_data/{$filename}");
